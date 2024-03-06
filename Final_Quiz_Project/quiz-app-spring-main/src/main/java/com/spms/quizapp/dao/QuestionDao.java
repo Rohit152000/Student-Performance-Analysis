282d96a5c1553ff023184fd4dad571c7ea3310e8
Rohit <155154783+Rohit152000@users.noreply.github.com>
2024-03-06 12:40:25 +0530

    Update QuestionDao.java

--- Final_Quiz_Project/quiz-app-spring-main/src/main/java/com/spms/quizapp/dao/QuestionDao.java
+++ Final_Quiz_Project/quiz-app-spring-main/src/main/java/com/spms/quizapp/dao/QuestionDao.java
@@ -15,7 +15,6 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
     @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
     
-//    @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
-//    List<Question> findRandomQuestionsByCategory(@Param("category") String category, @Param("numQ") int numQ);
+
 
 }  
